8be60d09152930b3f966caefcf2b81d8cc7e5e08
Dawid Lenkiewicz <dawid@ragnarson.com>
2016-04-01 22:04:44 +0200

    Improve intelligence of indexing data
    Because we wanted to improve intelligence of searching n-gram filter
    is added when analyzing data. It means that when importing data every
    word will be split into tokens. This allows us to search by parts
    (from 2 chars long) of any fields, not only by exact match of the
    whole word.
    
    Whitespace analyzer is used when searching happens. We don't want to
    tokenize our search text into n-grams, just do simple analysis with
    splitting on whitespace, lower-casing, and cleaning up non-standard
    characters (with asciifolding).

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -6,6 +6,39 @@ module Searchable
   included do
     include Elasticsearch::Model
 
+    settings analysis: {
+      filter: {
+        ngram_filter: {
+          type: "nGram",
+          min_gram: 2,
+          max_gram: 20
+        }
+      },
+      analyzer: {
+        ngram_analyzer: {
+          type: "custom",
+          tokenizer: "standard",
+          filter: [
+            "lowercase",
+            "asciifolding",
+            "ngram_filter"
+          ]
+        },
+        whitespace_analyzer: {
+          type: "custom",
+          tokenizer: "whitespace",
+          filter: [
+            "lowercase",
+            "asciifolding"
+          ]
+        }
+      }
+    } do
+      mappings _all: {
+        type: "string", analyzer: "ngram_analyzer", search_analyzer: "whitespace_analyzer"
+      }
+    end
+
     after_commit do
       __elasticsearch__.index_document
     end
